3133a8d03e7f02aef1d4834162b790086307c223
swasher <mr.swasher@gmail.com>
2021-10-18 18:54:18 +0300

    work

--- test_numpy.py
+++ test_numpy.py
@@ -1,1 +1,27 @@
-pass
+import numpy as np
+import icecream as ic
+
+class Cell(object):
+    x = 0
+    y = 0
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+    def __repr__(self):
+        return f'{x}:{y}'
+
+row = 3
+col = 3
+a = np.empty(shape=(col, row), dtype=Cell)
+
+for y in range(row):
+    for x in range(col):
+        c = Cell(x, y)
+        print(c)
+        a[x, y] = c
+        print(a[x, y])
+        print()
+
+print(a)
+print(a[0][0])
+print(a[1][1])
