457c0aef663158f56cd5fd4d5bc52122e7c5aca1
swasher <mr.swasher@gmail.com>
2021-11-15 11:01:38 +0300

    force merge

---
+++ asset_16_2560x1440/asset.py
@@ -0,0 +1,15 @@
+# add pixels to cells size for get entire game board:
+# left - 18, top - 81, right - 18, boottom - 17
+
+left = 11
+right = 14
+top = 58
+bottom = 13
+smile_y_coord = 27
+allow_noguess = True
+flag = 'right'
+open = 'left'
+nearby = 'left'
+
+# float Lag for the board to have time to update the image after click the mouse button. Different value for different game realization.
+LAG = 0.1
