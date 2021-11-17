93be28f78379ee24493dfe315007634104618f58
Swasher <mr.swasher@gmail.com>
2021-11-17 07:19:07 +0300

    evening job

--- main.py
+++ main.py
@@ -288,10 +288,10 @@ if __name__ == '__main__':
     # debug - test perfomance
     # print(timeit.Timer(matrix.bomb_counter2).timeit(number=100))
 
-    strategies = [solver_B1, solver_E1, solver_B2, solver_E2, solver_R1]
-    # strategies = [solver_B1E1, solver_B2, solver_E2, solver_R1]
+    # strategies = [solver_B1, solver_E1, solver_B2, solver_E2, solver_R1]
+    strategies = [solver_B1E1, solver_B2, solver_E2, solver_R1]
 
-    config.human = True
+    config.human = False
     if config.human:  # режим 'human'
         strategies.remove(solver_R1)
         strategies.append(solver_human)
