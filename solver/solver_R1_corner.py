da971ff82dfd8e698b059b542db1de6fd9dd1e1c
Swasher <mr.swasher@gmail.com>
2021-11-22 02:14:12 +0300

    commit

---
+++ solver/solver_R1_corner.py
@@ -0,0 +1,35 @@
+import numpy as np
+from asset import Asset
+from random import randrange
+
+
+def solver_R1_corner(matrix):
+    """
+    Нажимает рандомную клетку из закрытых.
+    Первые 4 - это углы.
+    :param matrix:
+    :return:
+    """
+
+    corner_cells = [matrix.table[0, 0],
+                    matrix.table[0, matrix.width-1],
+                    matrix.table[matrix.height-1, 0],
+                    matrix.table[matrix.height-1, matrix.width-1]
+                    ]
+
+    for cell in corner_cells:
+        if len(matrix.around_closed_cells(cell)) == 3 and cell.is_closed:
+            return [cell], Asset.open
+    else:
+        cells = matrix.get_closed_cells()
+        qty = len(cells)
+        random_cell = cells[randrange(qty)]
+        return [random_cell], Asset.open
+
+    # if config.turn_by_turn:
+    #     ic('------ R1-corner')
+    #     ic(random_cell)
+    #     random_cell.mark_cell_debug()
+    #     input("Press Enter to mouse moving")
+
+
